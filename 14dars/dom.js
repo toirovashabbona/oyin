bb58e9f8a9a457d5b57cf1e99a0c369fb9fae5d6
toirovashabbona <toirovashabbona167@gmail.com>
2024-06-19 07:47:38 +0500

    Add files via upload

---
+++ 14dars/dom.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+// var div = document.querySelector('.text');
+// var h1 = document.querySelectorAll('h1');
+// var text_h = document.getElementById("btn");
+// var text_h1 = document.querySelector(".text_h1");
+
+
+
+// var button_one = document.getElementById("btn_one");
+// var button_two = document.getElementById("btn_two");
+// var button_three = document.getElementById("btn_three");
+// var body = document.querySelector("body");
+
+// button_one.onclick =  function(){
+//     body.style.backgroundColor = "red"
+// }
+// button_two.onclick = function(){
+//     body.style.backgroundColor = "gold"
+// }
+// button_three.onclick = function(){
+//     body.style.backgroundColor = "green"
+// }
+
+
+var one = document.getElementById("h1");
+var two = document.getElementById("h2");
+var three = document.getElementById("h3");
+var four = document.getElementById("h4");
+var five = document.getElementById("h5");
+var six = document.getElementById("h6");
+
+one.onclick = function(){
+     document.write("Sizning balingiz 1 ball")
+}
+two.onclick = function(){
+    document.write("Sizning balingiz 2 ball")
+}
+three.onclick = function(){
+    document.write("Sizning balingiz 3 ball")
+}
+four.onclick = function(){
+    document.write("Sizning balingiz yoq")
+}
+five.onclick = function(){
+    document.write("Sizning balingiz 5 ball")
+}
+six.onclick = function(){
+    document.write("Sizning balingiz 6 ball")
+}
